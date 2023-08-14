76718fee578d7f10d0a89494fa7d80e09eff0286
ndurivin <ndurivo@gmail.com>
2023-08-14 20:10:27 +0300

    Adds bootstrap css to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  root "main#index"
+  get "about" => "about#index"
+
+
 end
