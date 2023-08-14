04fbd7eea3f5953ad443bd9b046062db54f3d4f7
ndurivin <ndurivo@gmail.com>
2023-08-14 23:15:10 +0300

    Adds other routes, controller and page views

---
+++ app/controllers/webdev_controller.rb
@@ -0,0 +1,4 @@
+class WebdevController < ApplicationController
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   root "main#index"
-  get "about" => "about#index"
+  get "about" => "about#index", as: :about
+  get "projects" => "project#index", as: :project
+  get "contact" => "contact#index", as: :contact
+
+  get "webdev" => "webdev#index", as: :web
+  get "design" => "design#index", as: :design
+  get "pentesting" => "pentesting#index", as: :pentesting
+  get "support" => "support#index", as: :support
+
 
 
 end
